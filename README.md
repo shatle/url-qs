64c7b724a22a73a54e87ea1c86a6a147ce79732d
shatle <shatle@qq.com>
2017-11-25 20:14:40 +0800

    change array transfer

--- README.md
+++ README.md
@@ -10,6 +10,14 @@ test('parse to plain object', t => {
     t.deepEqual(fn.parse('a=1&b=2'), {a: '1', b: '2'})
 })
 
+test('stringify object with array', t => {
+    t.deepEqual(fn.stringify({a: 1, b: [2, 3]}), "a=1&b%5B%5D=2&b%5B%5D=3")
+})
+
+test('stringify object with array 2', t => {
+    t.deepEqual(fn.stringify({a: 1, b: [2, 3]}), "a=1&b%5B%5D=2&b%5B%5D=3")
+})
+
 # complicate object
 test('stringify complicate object', t => {
     t.deepEqual(fn.stringify({
@@ -17,7 +25,7 @@ test('stringify complicate object', t => {
         b: [2, 3], 
         c: {d: 4, e: [5, 6]},
         f: {g: 7, h: 8}
-    }), "a=1&b%5B0%5D=2&b%5B1%5D=3&c%5Bd%5D=4&c%5Be%5D%5B0%5D=5&c%5Be%5D%5B1%5D=6&f%5Bg%5D=7&f%5Bh%5D=8")
+    }), "a=1&b%5B%5D=2&b%5B%5D=3&c%5Bd%5D=4&c%5Be%5D%5B%5D=5&c%5Be%5D%5B%5D=6&f%5Bg%5D=7&f%5Bh%5D=8")
 })
 ```
 
--- test.js
+++ test.js
@@ -5,8 +5,8 @@ test('stringify plain object', t => {
     t.deepEqual(fn.stringify({a: 1, b: 2}), "a=1&b=2")
 })
 
-test('stringify object with array', t => {
-    t.deepEqual(fn.stringify({a: 1, b: [2, 3]}), "a=1&b%5B0%5D=2&b%5B1%5D=3")
+test('stringify object with array 2', t => {
+    t.deepEqual(fn.stringify({a: 1, b: [2, 3]}), "a=1&b%5B%5D=2&b%5B%5D=3")
 })
 
 test('stringify complicate object', t => {
@@ -15,7 +15,7 @@ test('stringify complicate object', t => {
         b: [2, 3], 
         c: {d: 4, e: [5, 6]},
         f: {g: 7, h: 8}
-    }), "a=1&b%5B0%5D=2&b%5B1%5D=3&c%5Bd%5D=4&c%5Be%5D%5B0%5D=5&c%5Be%5D%5B1%5D=6&f%5Bg%5D=7&f%5Bh%5D=8")
+    }), "a=1&b%5B%5D=2&b%5B%5D=3&c%5Bd%5D=4&c%5Be%5D%5B%5D=5&c%5Be%5D%5B%5D=6&f%5Bg%5D=7&f%5Bh%5D=8")
 })
 
 test('parse to plain object', t => {
@@ -23,11 +23,11 @@ test('parse to plain object', t => {
 })
 
 test('parse to object with array', t => {
-    t.deepEqual(fn.parse('a=1&b%5B0%5D=2&b%5B1%5D=3'), {a: '1', b: ['2', '3']})
+    t.deepEqual(fn.parse('a=1&b%5B%5D=2&b%5B%5D=3'), {a: '1', b: ['2', '3']})
 })
 
 test('parse to complicate object', t => {
-    t.deepEqual(fn.parse('a=1&b%5B0%5D=2&b%5B1%5D=3&c%5Bd%5D=4&c%5Be%5D%5B0%5D=5&c%5Be%5D%5B1%5D=6&f%5Bg%5D=7&f%5Bh%5D=8'), {
+    t.deepEqual(fn.parse('a=1&b%5B%5D=2&b%5B%5D=3&c%5Bd%5D=4&c%5Be%5D%5B%5D=5&c%5Be%5D%5B%5D=6&f%5Bg%5D=7&f%5Bh%5D=8'), {
         a: '1', 
         b: ['2', '3'], 
         c: {d: '4', e: ['5', '6']},
